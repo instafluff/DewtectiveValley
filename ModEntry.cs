90a34f7ada1613ecfa6980d7c99e2d2b94050d80
Instafluff <35773245+instafluff@users.noreply.github.com>
2020-10-13 13:26:07 -0700

    Dynamically Changing Character Dialogue
    Using Tokens!

--- ModEntry.cs
+++ ModEntry.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,11 +23,31 @@ namespace DewtectiveValley
 			helper.Events.Input.ButtonReleased += Input_ButtonReleased;
 			helper.Events.GameLoop.UpdateTicking += GameLoop_UpdateTicking;
 			helper.Events.GameLoop.UpdateTicked += GameLoop_UpdateTicked;
-        }
+
+			//// create with random manifest data
+			//IContentPack contentPack = this.Helper.ContentPacks.CreateFake(
+			//   directoryPath: Path.Combine( this.Helper.DirectoryPath, "content-pack" ),
+			//);
+
+			//// create with given manifest fields
+			//IContentPack contentPack = this.Helper.ContentPacks.CreateTemporary(
+			//   directoryPath: Path.Combine( this.Helper.DirectoryPath, "content-pack" ),
+			//   id: Guid.NewGuid().ToString( "N" ),
+			//   name: "temporary content pack",
+			//   description: "...",
+			//   author: "...",
+			//   version: new SemanticVersion( 1, 0, 0 )
+			//);
+		}
+
+		string[] characters = new string[] { "Abigail", "Shane", "Willy", "Gus" };
+		string[] numbers = new string[] { "Zero", "One", "Two", "Three", "Four", "Five" };
+		Dictionary<string, string> dialogue = new Dictionary<string, string>();
 
 		private void GameLoop_GameLaunched( object sender, GameLaunchedEventArgs e )
 		{
 			Console.WriteLine( "Game Has Launched" );
+
 			var api = this.Helper.ModRegistry.GetApi<IContentPatcherAPI>( "Pathoschild.ContentPatcher" );
 			api.RegisterToken( this.ModManifest, "PlayerName", () =>
 			{
@@ -36,6 +57,35 @@ namespace DewtectiveValley
 					return new[] { SaveGame.loaded.player.Name }; // lets token be used before save is fully loaded
 				return null;
 			} );
+
+			foreach( var character in characters )
+			{
+				dialogue.Add( character, string.Format( "TESTING {0} DIALOGUE", character ) );
+				api.RegisterToken( this.ModManifest, character + "Intro", () => {
+					var ch = character;
+					if( Context.IsWorldReady )
+						return new[] { dialogue[ ch ] };
+					if( SaveGame.loaded?.player != null )
+						return new[] { dialogue[ ch ] }; // lets token be used before save is fully loaded
+					return null;
+				} );
+				for( var i = 0; i < 6; i++ )
+				{
+					api.RegisterToken( this.ModManifest, character + numbers[ i ], () => {
+						var ch = character;
+						if( Context.IsWorldReady )
+							return new[] { dialogue[ ch ] };
+						if( SaveGame.loaded?.player != null )
+							return new[] { dialogue[ ch ] }; // lets token be used before save is fully loaded
+						return null;
+					} );
+				}
+			}
+
+			foreach( IContentPack contentPack in this.Helper.ContentPacks.GetOwned() )
+			{
+				this.Monitor.Log( $"Reading content pack: {contentPack.Manifest.Name} {contentPack.Manifest.Version} from {contentPack.DirectoryPath}" );
+			}
 		}
 
 		private void Input_ButtonPressed( object sender, ButtonPressedEventArgs e )
